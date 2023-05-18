aafc7fade50d764d66742adbc836e7de831860d0
jaimemin <jaimemin@gmail.com>
2023-05-18 23:57:43 +0900

    Spring Data Jpa Repository Mock

--- src/test/java/com/tistory/jaimemin/springdatajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/tistory/jaimemin/springdatajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +28,31 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member memberA = new Member("memberA");
+        Member memberB = new Member("memberB");
+        memberJpaRepository.save(memberA);
+        memberJpaRepository.save(memberB);
+
+        Member findMemberA = memberJpaRepository.findById(memberA.getId()).get();
+        Member findMemberB = memberJpaRepository.findById(memberB.getId()).get();
+        assertThat(findMemberA).isEqualTo(memberA);
+        assertThat(findMemberB).isEqualTo(memberB);
+
+        List<Member> members = memberJpaRepository.findAll();
+        assertThat(members.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(memberA);
+        memberJpaRepository.delete(memberB);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 
 }
